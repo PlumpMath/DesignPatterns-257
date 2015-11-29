78638a189defe1bc0b5aa829886ccfc0f0833e2c
michal_urbanski <michal_urbanski@windowslive.com>
2015-11-29 16:53:08 +0100

    Removes not needed class, creates folders structure and adds proper nunit version

---
+++ DesignPatterns/Tests/SingletonTests.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace DesignPatterns.Tests
+{
+    [TestFixture]
+    public class SingletonTests
+    {
+        [Test]
+        public void Test_OnlyOneObjectReference_Exists()
+        {
+            Assert.Inconclusive(); 
+        }
+    }
+}
