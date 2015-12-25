ec338b4c01a5742ce7f5b84e3aa19995e9f55d89
michal_urbanski <michal_urbanski@windowslive.com>
2015-12-25 20:54:52 +0100

    Adds sandwich model for builder pattern

---
+++ DesignPatterns/BuilderPattern/Sandwich.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace DesignPatterns.BuilderPattern
+{
+    public class Sandwich
+    {
+        private CheeseType _cheeseType;
+        private bool _hasKetchup;
+        private bool _hasMustard;
+        private MeatType _meatType;
+        private List<string> _vegatables;
+
+        // This constructor has a problem - too many parameters
+        public Sandwich(CheeseType cheeseType, MeatType meatType, bool hasKetchup, bool hasMustard, IEnumerable<string> vegetables)
+        {
+            _cheeseType = cheeseType;
+            _meatType = meatType;
+            _hasKetchup = hasKetchup;
+            _hasMustard = hasMustard;
+            _vegatables = vegetables.ToList(); 
+        }
+    }
+
+    public enum CheeseType
+    {
+        Cheddar, 
+        Gouda, 
+        Emmentaler
+    }
+
+    public enum MeatType
+    {
+        Sausage, 
+        Pork, 
+        Chicken
+    }
+}
